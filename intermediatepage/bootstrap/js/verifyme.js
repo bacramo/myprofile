a05fc3a438822b1c7a028f2700e7996644acab4c
bacramo <38225355+bacramo@users.noreply.github.com>
2018-10-23 13:29:39 +0300

    new update

---
+++ intermediatepage/bootstrap/js/verifyme.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+function myverification() {
+
+    var theanswer = document.getElementById("verifyanswer").value;
+    if (theanswer == "your name") {
+        window.alert("Welcome to my real me.");
+
+        window.location = ".././././v2secret.html";
+    }
+    else {
+        window.location = ".././././v2offic.html";
+        window.alert("Sorry. Wrong answer.");
+    }
+}
